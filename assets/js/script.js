e59905dc189294a18df631da0e61a0f0b4d3ce7d
4jr <aapfjr@gmail.com>
2025-12-01 02:58:07 -0800

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-const intervalo = 300000;
-setInterval(() => {
-    const iframe = document.getElementById("calendario");
-    const baseUrl = iframe.src.split("?")[0];
-    const params = iframe.src.split("?")[1];
-    iframe.src = baseUrl + "?" + params + "&t=" + Date.now();
-}, intervalo);
